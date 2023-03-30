eb52b8b8a8c91c808427245879fec7b62610cc30
Marko Sanković <msankovic@synapticon.com>
2023-03-30 10:42:26 +0200

    Add start monitoring

---
+++ src/start-monitoring.ts
@@ -0,0 +1,24 @@
+import { client } from './init-client';
+import { first, mergeMap, Subscription } from 'rxjs';
+import { DeviceRef } from 'motion-master-client';
+
+let subscription: Subscription;
+
+process.on('SIGINT', function () {
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C).\nStopping monitoring and closing sockets.");
+  subscription?.unsubscribe();
+  client.closeSockets();
+  process.exit(0);
+});
+
+const ids: [DeviceRef, number, number][] = [
+  [0, 0x2030, 1], // Core temperature / Measured temperature
+  [0, 0x2031, 1], // Drive temperature / Measured temperature
+];
+
+subscription = client.reqResSocket.opened$.pipe(
+  first(Boolean),
+  mergeMap(() => client.startMonitoring(ids, 1000000)),
+).subscribe((values) => {
+  console.log(values);
+});
