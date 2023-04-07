131f1d5c6860ca078d9aa105a7992f7c78eb6bf5
Marko Sanković <msankovic@synapticon.com>
2023-04-07 16:47:20 +0200

    Refactor start full auto tuning

--- src/start-full-auto-tuning.rx.ts
+++ src/start-full-auto-tuning.rx.ts
@@ -1,6 +1,7 @@
 import { Device } from 'motion-master-client/src/lib/device';
 import { client } from './init-client';
 import { first, mergeMap, combineLatest, tap } from 'rxjs';
+import { MotionMasterMessage, RequestStatus } from 'motion-master-client';
 
 let devices: Device[];
 
@@ -11,20 +12,22 @@ client.socketsOpened$.pipe(
   mergeMap((devices) => combineLatest(devices.map(({ deviceAddress }) => client.request.startFullAutoTuning({ deviceAddress, controllerType: 0 }, 60000)))),
 ).subscribe({
   next: (statuses) => {
-    const json = JSON.stringify(
-      statuses.map((status, index) => {
-        const id = devices[index]?.id;
-        if (status.request === 'succeeded') {
-          return `${id}: 100%, dampingRatio: ${status.dampingRatio}, settlingTime: ${status.settlingTime}`;
-        } else if (status.request === 'failed') {
-          return `${id}: FAILED`;
-        } else if (status.request === 'running') {
-          return `${id}: ${status.progress?.percentage ?? 0}%`;
-        }
-      })
-    );
+    const json = JSON.stringify(mapStatusesToMessages(statuses));
     console.clear();
     console.log(json);
   },
   complete: () => client.closeSockets(),
 });
+
+function mapStatusesToMessages(statuses: (MotionMasterMessage.Status.FullAutoTuning & { messageId: string, request: RequestStatus })[]) {
+  return statuses.map((status, index) => {
+    const id = devices[index]?.id;
+    if (status.request === 'succeeded') {
+      return `${id}: 100%, dampingRatio: ${status.dampingRatio}, settlingTime: ${status.settlingTime}`;
+    } else if (status.request === 'failed') {
+      return `${id}: FAILED`;
+    } else if (status.request === 'running') {
+      return `${id}: ${status.progress?.percentage ?? 0}%`;
+    }
+  });
+}
