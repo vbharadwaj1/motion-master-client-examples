4105f7c9681d9bd317f1f9ffef67e72edfcf3544
Marko Sanković <msankovic@synapticon.com>
2023-04-07 16:42:59 +0200

    Update start full auto tuning

--- src/start-full-auto-tuning.rx.ts
+++ src/start-full-auto-tuning.rx.ts
@@ -1,20 +1,25 @@
+import { Device } from 'motion-master-client/src/lib/device';
 import { client } from './init-client';
-import { first, mergeMap, combineLatest } from 'rxjs';
+import { first, mergeMap, combineLatest, tap } from 'rxjs';
+
+let devices: Device[];
 
 client.socketsOpened$.pipe(
   first(Boolean),
   mergeMap(() => client.request.getDevices(3000)),
+  tap((d) => { devices = d }),
   mergeMap((devices) => combineLatest(devices.map(({ deviceAddress }) => client.request.startFullAutoTuning({ deviceAddress, controllerType: 0 }, 60000)))),
 ).subscribe({
   next: (statuses) => {
     const json = JSON.stringify(
-      statuses.map((status) => {
+      statuses.map((status, index) => {
+        const id = devices[index]?.id;
         if (status.request === 'succeeded') {
-          return `messageId: ${status.messageId}, progress: 100%, dampingRatio: ${status.dampingRatio}, settlingTime: ${status.settlingTime}`;
+          return `${id}: 100%, dampingRatio: ${status.dampingRatio}, settlingTime: ${status.settlingTime}`;
         } else if (status.request === 'failed') {
-          return `messageId: ${status.messageId}, FAILED`;
+          return `${id}: FAILED`;
         } else if (status.request === 'running') {
-          return `messageId: ${status.messageId}, progress: ${status.progress?.percentage ?? 0}%`;
+          return `${id}: ${status.progress?.percentage ?? 0}%`;
         }
       })
     );
