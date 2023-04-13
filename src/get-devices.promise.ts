3f27cb8c7d090ba2d5e6bd484857b9d20e5876df
Marko Sanković <msankovic@synapticon.com>
2023-04-13 11:38:48 +0200

    Improve getDevicesPromise

--- src/get-devices.promise.ts
+++ src/get-devices.promise.ts
@@ -1,12 +1,18 @@
 import { client } from './init-client';
 import { firstValueFrom } from 'rxjs';
 
-(async function () {
-  await client.whenReady();
-
+client.whenReady().then(async () => {
   const devices = await firstValueFrom(client.request.getDevices());
   const json = JSON.stringify(devices, null, 2);
   console.log(json);
+}).finally(() => client.closeSockets());
 
-  client.closeSockets();
-})();
+// (async function () {
+//   await client.whenReady();
+
+//   const devices = await firstValueFrom(client.request.getDevices());
+//   const json = JSON.stringify(devices, null, 2);
+//   console.log(json);
+
+//   client.closeSockets();
+// })();
