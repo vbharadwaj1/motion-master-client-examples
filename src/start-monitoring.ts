13d935cd9fcc08b659e321cc6f18c0750ec21925
Marko Sanković <msankovic@synapticon.com>
2023-05-15 23:39:22 +0200

    Change the list of monitoring parameters

--- src/start-monitoring.ts
+++ src/start-monitoring.ts
@@ -13,18 +13,20 @@ process.on('SIGINT', function () {
 
 const ids: [number, number, number][] = [
   [0, 0x20F0, 0], // Timestamp
+  [0, 0x6040, 0], // Controlword
   [0, 0x6041, 0], // Statusword
+  [0, 0x6060, 0], // Modes of operation
   [0, 0x6061, 0], // Modes of operation display
   [0, 0x6064, 0], // Position actual value
   [0, 0x606C, 0], // Velocity actual value
   [0, 0x6077, 0], // Torque actual value
-  [0, 0x2401, 0], // Analog input 1
-  [0, 0x2402, 0], // Analog input 1
-  [0, 0x2403, 0], // Analog input 1
-  [0, 0x2404, 0], // Analog input 1
-  [0, 0x2702, 0], // Tuning status
-  [0, 0x60FD, 0], // Digital inputs
-  [0, 0x2704, 0], // User MISO
+  // [0, 0x2401, 0], // Analog input 1
+  // [0, 0x2402, 0], // Analog input 1
+  // [0, 0x2403, 0], // Analog input 1
+  // [0, 0x2404, 0], // Analog input 1
+  // [0, 0x2702, 0], // Tuning status
+  // [0, 0x60FD, 0], // Digital inputs
+  // [0, 0x2704, 0], // User MISO
   [0, 0x60FC, 0], // Position demand internal value
   [0, 0x606B, 0], // Velocity demand value
   [0, 0x6074, 0], // Torque demand
