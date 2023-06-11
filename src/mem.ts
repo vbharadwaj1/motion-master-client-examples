56ad2b41621bbf27f2fd998add71c3886be20523
Marko Sanković <msankovic@synapticon.com>
2023-06-11 14:30:00 +0200

    Update mem.ts

--- src/mem.ts
+++ src/mem.ts
@@ -14,7 +14,9 @@ subscription = client.onceReady$.pipe(
   mergeMap(() => client.startMonitoringValue([0, 0x6064, 0], 500000)),
   bufferCount(6),
   map((values) => {
-    const sum = values.map(v => Math.abs(v)).reduce((p, v) => (p + v), 0);
+    const sum = values
+      .map(v => Math.abs(v))
+      .reduce((p, v) => (p + v), 0);
     const avg = sum / values.length;
     const pos = avg % 4096;
     const val = pos / (4096 / 26);
