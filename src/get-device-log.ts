a9f8a2d7cc6ad76c052083ab57e8dc3697563318
Marko Sanković <msankovic@synapticon.com>
2023-06-13 10:51:49 +0200

    Improve get device log

--- src/get-device-log.ts
+++ src/get-device-log.ts
@@ -1,11 +1,16 @@
-import { client } from './init-client';
+import { program } from 'commander';
 import { firstValueFrom } from 'rxjs';
+import { client } from './init-client';
+
+program.parse();
+
+const { deviceRef } = program.opts();
 
 client.whenReady().then(async () => {
-  const currLog = await firstValueFrom(client.request.getDecodedFile(0, 'logging_curr.log'));
+  const currLog = await firstValueFrom(client.request.getDecodedFile(deviceRef, 'logging_curr.log'));
   console.log(currLog);
 
-  const prevLog = await firstValueFrom(client.request.getDecodedFile(0, 'logging_prev.log'));
+  const prevLog = await firstValueFrom(client.request.getDecodedFile(deviceRef, 'logging_prev.log'));
   console.log(prevLog);
 
   client.closeSockets();
