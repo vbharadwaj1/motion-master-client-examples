bf5b265eb3cb014759a22515bcf15656cdcffbcb
Marko Sanković <msankovic@synapticon.com>
2023-06-15 12:38:29 +0200

    Add say function to mem

--- src/mem.ts
+++ src/mem.ts
@@ -1,5 +1,11 @@
+import { program } from 'commander';
 import { client } from './init-client';
-import { bufferCount, distinctUntilChanged, filter, map, mergeMap, Subscription, } from 'rxjs';
+import { bufferCount, distinctUntilChanged, filter, lastValueFrom, map, mergeMap, Subscription, } from 'rxjs';
+import { Cia402State, resolveAfter } from 'motion-master-client';
+
+program.parse();
+
+const { deviceRef } = program.opts();
 
 let subscription: Subscription;
 
@@ -12,15 +18,16 @@ process.on('SIGINT', function () {
 
 const samplingInterval = 250000; // microseconds
 const resolution = 524288; // ticks
-const ticksPerChar = resolution / 26;
+const ticksPerChar = Math.round(resolution / 26);
+const standardDeviationMargin = ticksPerChar / 4;
 const samplingSize = 6;
 
 subscription = client.onceReady$.pipe(
-  mergeMap(() => client.startMonitoringValue([0, 0x6064, 0], samplingInterval)),
+  mergeMap(() => client.startMonitoringValue([deviceRef, 0x6064, 0], samplingInterval)),
   bufferCount(samplingSize),
   filter((values) => {
     const standardDeviation = calculateStandardDeviation(values);
-    return standardDeviation < ticksPerChar;
+    return standardDeviation < standardDeviationMargin;
   }),
   map((values) => calculateCharPosition(values)),
   distinctUntilChanged(),
@@ -28,11 +35,7 @@ subscription = client.onceReady$.pipe(
   console.log(String.fromCharCode(65 + charPosition));
 });
 
-function calculateZScore(dataPoint: number, mean: number, standardDeviation: number) {
-  return (dataPoint - mean) / standardDeviation;
-}
-
-function calculateStandardDeviation(data: number[]) {
+function calculateStandardDeviation(data: number[]): number {
   const mean = calculateMean(data);
   const squaredDifferences = data.map((value) => Math.pow(value - mean, 2));
   const averageSquaredDifference = calculateMean(squaredDifferences);
@@ -40,16 +43,54 @@ function calculateStandardDeviation(data: number[]) {
   return standardDeviation;
 }
 
-function calculateMean(data: number[]) {
+function calculateMean(data: number[]): number {
   const sum = data.reduce((accumulator, value) => accumulator + value, 0);
   const mean = sum / data.length;
   return mean;
 }
 
-function calculateCharPosition(data: number[]) {
+function calculateCharPosition(data: number[]): number {
   const sum = data.map(v => Math.abs(v)).reduce((p, v) => (p + v), 0);
   const avg = sum / data.length;
   const pos = avg % resolution;
   const val = pos / ticksPerChar;
   return Math.floor(val);
 }
+
+async function say(message: string): Promise<void> {
+  const chars = message.toUpperCase().split('');
+  const positionActualValue = await client.request.upload(deviceRef, 0x6064, 0);
+  const charToPositionMap = createCharToPositionMap();
+
+  await client.request.downloadMany([
+    [deviceRef, 0x6060, 0, 1], // profile position mode
+    [deviceRef, 0x6067, 0, 250], // position window
+    [deviceRef, 0x607A, 0, positionActualValue], // target position
+    [deviceRef, 0x6081, 0, 100000], // profile velocity
+    [deviceRef, 0x6083, 0, 10000], // profile acceleration
+    [deviceRef, 0x6084, 0, 10000], // profile deceleration
+  ]);
+
+  await client.request.transitionToCia402State(deviceRef, Cia402State.OPERATION_ENABLED);
+
+  for (const c of chars) {
+    await client.request.download(deviceRef, 0x607A, 0, charToPositionMap[c]);
+    await client.request.applySetPoint(deviceRef);
+    await resolveAfter(50);
+    await client.whenTargetReached(deviceRef);
+    await resolveAfter(3000);
+  }
+
+  return;
+}
+
+function createCharToPositionMap(): { [char: string]: number } {
+  const val = Math.round(ticksPerChar / 2);
+  const obj: { [char: string]: number } = {};
+  for (let i = 0; i < 26; i++) {
+    obj[String.fromCharCode(65 + i)] = i * ticksPerChar + val;
+  }
+  return obj;
+}
+
+say('TEST').then(() => client.closeSockets());
--- src/profile-position-mode.promise.ts
+++ src/profile-position-mode.promise.ts
@@ -1,7 +1,6 @@
 import { program } from 'commander';
 import { client } from './init-client';
 import { Cia402State } from 'motion-master-client';
-import { lastValueFrom } from 'rxjs';
 
 program.parse();
 
@@ -10,13 +9,16 @@ const { deviceRef } = program.opts();
 client.whenReady().then(async () => {
   await client.request.resetTargets(deviceRef);
 
-  const positionActualValue = await client.request.upload(deviceRef, 0x6064, 0);
+  let positionActualValue = await client.request.upload(deviceRef, 0x6064, 0);
+  console.log(`Position actual value is ${positionActualValue}.`);
   const resolution = await client.request.upload(deviceRef, 0x2110, 3); // 524288
+  console.log(`Resolution is ${resolution}.`);
   const targetPosition = positionActualValue + resolution; // one full rotation
+  console.log(`Target position is ${targetPosition} (one full rotation)`);
 
   await client.request.downloadMany([
     [deviceRef, 0x6060, 0, 1], // profile position mode
-    [deviceRef, 0x6067, 0, 500], // position window
+    [deviceRef, 0x6067, 0, 10], // position window
     [deviceRef, 0x607A, 0, targetPosition], // target position
     [deviceRef, 0x6081, 0, 10000], // profile velocity
     [deviceRef, 0x6083, 0, 10000], // profile acceleration
@@ -24,8 +26,8 @@ client.whenReady().then(async () => {
   ]);
 
   await client.request.transitionToCia402State(deviceRef, Cia402State.OPERATION_ENABLED);
-  await client.request.applySetPoints(deviceRef);
-
-  await lastValueFrom(client.waitUntilTargetReached(deviceRef));
-  console.log('tr - target reached');
+  await client.request.applySetPoint(deviceRef);
+  await client.whenTargetReached(deviceRef);
+  positionActualValue = await client.request.upload(deviceRef, 0x6064, 0);
+  console.log(`Target reached! Position actual value is ${positionActualValue}.`);
 }).finally(() => client.closeSockets());
