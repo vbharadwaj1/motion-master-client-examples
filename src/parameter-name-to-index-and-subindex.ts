630f4ea22d91a5f362e16f7e2a31778320b81aa7
Marko Sanković <msankovic@synapticon.com>
2023-11-30 15:27:50 +0100

    Fix: parameter name to index and subindex

--- src/parameter-name-to-index-and-subindex.ts
+++ src/parameter-name-to-index-and-subindex.ts
@@ -6,7 +6,8 @@ client.onceReady$.pipe(
 ).subscribe({
   next: (status) => {
     const obj = status.parameters.reduce((acc, parameter) => {
-      acc[parameter.name] = [parameter.index, parameter.subindex]; // NOTE: duplicate names will be overwritten
+      const name = parameter.group ? `${parameter.group}: ${parameter.name}` : parameter.name;
+      acc[name] = [parameter.index, parameter.subindex];
       return acc;
     }, {} as { [key: string]: [number, number] });
     console.log(JSON.stringify(obj));
