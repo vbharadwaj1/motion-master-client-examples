7f1736e14015d5e3d0115963c94f5e42b3c5a0ed
Marko Sanković <msankovic@synapticon.com>
2023-12-12 10:40:21 +0100

    Improve set-device-file

---
+++ src/request/set-device-file.promise.ts
@@ -0,0 +1,28 @@
+import { Argument, program } from 'commander';
+import { makeDeviceRefObj } from 'motion-master-client';
+import { readFileSync } from 'fs';
+import { firstValueFrom } from 'rxjs';
+import { client, logStringifiedStatus } from '../init-client';
+
+program
+  .addArgument(new Argument('<name>', 'file name, e.g. config.csv'))
+  .addArgument(new Argument('<path>', 'Path to a file to write onto the device\'s flash'))
+  .addArgument(new Argument('[overwrite]', 'overwrite the existing file with the same name')
+    .argOptional()
+    .default(false)
+    .choices(['true', 'false'])
+    .argParser((value) => value.toLowerCase() === 'true'));
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [name, path, overwrite = false] = program.processedArgs;
+
+const content = Buffer.from(readFileSync(path, { encoding: 'utf-8' }), 'utf-8');
+
+client.whenReady().then(async () => {
+  const status = await firstValueFrom(client.request.setDeviceFile({ ...deviceRefObj, name, content, overwrite }, requestTimeout, messageId));
+  logStringifiedStatus(status);
+}).finally(() => client.closeSockets());
+
---
+++ src/request/set-device-file.rx.ts
@@ -0,0 +1,29 @@
+import { Argument, program } from 'commander';
+import { makeDeviceRefObj } from 'motion-master-client';
+import { readFileSync } from 'fs';
+import { mergeMap } from 'rxjs';
+import { client, logStringifiedStatus } from '../init-client';
+
+program
+  .addArgument(new Argument('<name>', 'file name, e.g. config.csv'))
+  .addArgument(new Argument('<path>', 'Path to a file to write onto the device\'s flash'))
+  .addArgument(new Argument('[overwrite]', 'overwrite the existing file with the same name')
+    .argOptional()
+    .default(false)
+    .choices(['true', 'false'])
+    .argParser((value) => value.toLowerCase() === 'true'));
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [name, path, overwrite = false] = program.processedArgs;
+
+const content = Buffer.from(readFileSync(path, { encoding: 'utf-8' }), 'utf-8');
+
+client.onceReady$.pipe(
+  mergeMap(() => client.request.setDeviceFile({ ...deviceRefObj, name, content, overwrite }, requestTimeout, messageId)),
+).subscribe({
+  next: logStringifiedStatus,
+  complete: () => client.closeSockets(),
+});
