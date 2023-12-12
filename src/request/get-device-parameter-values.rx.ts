be943cdb0716d4f93e2705788381f2e4cd75e776
Marko Sanković <msankovic@synapticon.com>
2023-12-12 14:10:58 +0100

    Add get-device-parameter-values

---
+++ src/request/get-device-parameter-values.rx.ts
@@ -0,0 +1,36 @@
+import { Argument, program } from 'commander';
+import { MotionMasterMessage, makeDeviceRefObj, splitParameterId } from 'motion-master-client';
+import { client, logStringifiedStatus } from '../init-client';
+import { mergeMap } from 'rxjs';
+
+program
+  .addArgument(new Argument('<parameterIds>', 'comma-separated list of parameter ids, e.g. "0x2030:01,0x2110:0C"'))
+  .addArgument(new Argument('[sendProgress]', 'send progress while retrieving the parameter values from the device')
+    .argOptional()
+    .default(false)
+    .choices(['true', 'false'])
+    .argParser((value) => value.toLowerCase() === 'true'))
+  .addArgument(new Argument('[loadFromCache]', 'load parameter values from the Motion Master cache')
+    .argOptional()
+    .default(false)
+    .choices(['true', 'false'])
+    .argParser((value) => value.toLowerCase() === 'true'));
+
+program.parse();
+
+const { deviceRef, requestTimeout = 30000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [parameterIds, sendProgress, loadFromCache] = program.processedArgs as [string, boolean, boolean];
+
+const parameters = parameterIds.split(',').reduce((arr, parameterId) => {
+  const [index, subindex] = splitParameterId(parameterId.trim());
+  arr.push({ index, subindex, loadFromCache });
+  return arr;
+}, [] as MotionMasterMessage.Request.GetDeviceParameterValues.IParameter[]);
+
+client.onceReady$.pipe(
+  mergeMap(() => client.request.getDeviceParameterValues({ ...deviceRefObj, parameters, sendProgress }, requestTimeout, messageId))
+).subscribe({
+  next: logStringifiedStatus,
+  complete: () => client.closeSockets(),
+});
