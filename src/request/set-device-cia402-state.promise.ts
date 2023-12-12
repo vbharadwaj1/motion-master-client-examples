c70eb9cb5bbf075b8fb80cbe30e5cf1980b91b3a
Marko Sanković <msankovic@synapticon.com>
2023-12-12 11:57:57 +0100

    Add set-device-cia402-state

---
+++ src/request/set-device-cia402-state.promise.ts
@@ -0,0 +1,21 @@
+import { Argument, program } from 'commander';
+import { MotionMasterMessage, makeDeviceRefObj } from 'motion-master-client';
+import { client, logStringifiedStatus } from '../init-client';
+import { firstValueFrom } from 'rxjs';
+
+program.addArgument(
+  new Argument('<state>', 'CiA402 state')
+    .choices(Object.keys(MotionMasterMessage.Status.DeviceCiA402State.State))
+    .argParser((value: any) => MotionMasterMessage.Status.DeviceCiA402State.State[value])
+);
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [state] = program.processedArgs;
+
+client.whenReady().then(async () => {
+  const status = await firstValueFrom(client.request.setDeviceCia402State({ ...deviceRefObj, state }, requestTimeout, messageId));
+  logStringifiedStatus(status);
+}).finally(() => client.closeSockets());
---
+++ src/request/set-device-cia402-state.rx.ts
@@ -0,0 +1,23 @@
+import { Argument, program } from 'commander';
+import { MotionMasterMessage, makeDeviceRefObj } from 'motion-master-client';
+import { client, logStringifiedStatus } from '../init-client';
+import { mergeMap } from 'rxjs';
+
+program.addArgument(
+  new Argument('<state>', 'CiA402 state')
+    .choices(Object.keys(MotionMasterMessage.Status.DeviceCiA402State.State))
+    .argParser((value: any) => MotionMasterMessage.Status.DeviceCiA402State.State[value])
+);
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [state] = program.processedArgs;
+
+client.onceReady$.pipe(
+  mergeMap(() => client.request.setDeviceCia402State({ ...deviceRefObj, state }, requestTimeout, messageId)),
+).subscribe({
+  next: logStringifiedStatus,
+  complete: () => client.closeSockets(),
+});
