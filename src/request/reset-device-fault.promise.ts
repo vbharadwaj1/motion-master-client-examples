addc8ee6dcbb5c59e3e64a0dfce5420385e6d986
Marko Sanković <msankovic@synapticon.com>
2023-12-12 11:20:33 +0100

    Add reset-device-fault

---
+++ src/request/reset-device-fault.promise.ts
@@ -0,0 +1,14 @@
+import { program } from 'commander';
+import { makeDeviceRefObj } from 'motion-master-client';
+import { client, logStringifiedStatus } from '../init-client';
+import { firstValueFrom } from 'rxjs';
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+
+client.whenReady().then(async () => {
+  const status = await firstValueFrom(client.request.resetDeviceFault(deviceRefObj, requestTimeout, messageId));
+  logStringifiedStatus(status);
+}).finally(() => client.closeSockets());
