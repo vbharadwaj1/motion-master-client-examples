5a1a2d4bb975fb9596b9130c57953e27b7e3f8dd
Marko Sanković <msankovic@synapticon.com>
2023-12-12 10:06:50 +0100

    Improve get device parameter info

--- src/request/get-device-parameter-info.rx.ts
+++ src/request/get-device-parameter-info.rx.ts
@@ -1,16 +1,16 @@
 import { program } from 'commander';
 import { makeDeviceRefObj } from 'motion-master-client';
+import { client, logStringifiedStatus } from '../init-client';
 import { mergeMap } from 'rxjs';
-import { client } from './init-client';
 
 program.parse();
 
-const { deviceRef } = program.opts();
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
 const deviceRefObj = makeDeviceRefObj(deviceRef);
 
 client.onceReady$.pipe(
-  mergeMap(() => client.request.getDeviceParameterInfo(deviceRefObj, 5000)),
+  mergeMap(() => client.request.getDeviceParameterInfo(deviceRefObj, requestTimeout, messageId)),
 ).subscribe({
-  next: (status) => console.log(JSON.stringify(status.parameters, null, 2)),
+  next: logStringifiedStatus,
   complete: () => client.closeSockets(),
 });
