9b0dae5c837e4238579cb6c24dea35ba4e429c87
Marko Sanković <msankovic@synapticon.com>
2023-12-12 11:15:55 +0100

    Add delete-device-file

---
+++ src/request/delete-device-file.rx.ts
@@ -0,0 +1,19 @@
+import { Argument, program } from 'commander';
+import { makeDeviceRefObj } from 'motion-master-client';
+import { mergeMap } from 'rxjs';
+import { client, logStringifiedStatus } from '../init-client';
+
+program.addArgument(new Argument('<name>', 'file name, e.g. config.csv'));
+
+program.parse();
+
+const { deviceRef, requestTimeout = 5000, messageId } = program.opts();
+const deviceRefObj = makeDeviceRefObj(deviceRef);
+const [name] = program.processedArgs;
+
+client.onceReady$.pipe(
+  mergeMap(() => client.request.deleteDeviceFile({ ...deviceRefObj, name }, requestTimeout, messageId)),
+).subscribe({
+  next: logStringifiedStatus,
+  complete: () => client.closeSockets(),
+});
